2a2c54badffe26b105ef559578c8c62fbbae6f75
Francis <yuyan0@stanford.edu>
2016-05-22 13:55:05 -0700

    ScreamServer.cpp: add poll

--- scream/ScreamServer.cpp
+++ scream/ScreamServer.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
 #include <cstdlib>
+#include <cstdint>
+#include <poll.h>
 
 #include "socket.hh"
+#include "timestamp.hh" 
 #include "util.hh"
+
 #include "ScreamRx.h"
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
+void recvRtp(ScreamRx *screamRx, UDPSocket &socket) 
+{
+}
+
+int main(int argc, char *argv[]) 
+{
   if (argc != 2) {
     cerr << "Usage: " << argv[0] << " PORT" << endl; 
     return EXIT_FAILURE;
@@ -15,13 +24,21 @@ int main(int argc, char *argv[]) {
 
   UDPSocket socket;
   socket.bind(Address("0", argv[1]));
-
   cerr << "Listening on " << socket.local_address().to_string() << endl;
 
-  UDPSocket::received_datagram recd = socket.recv();
-  cout << recd.payload << endl;
-
   ScreamRx *screamRx = new ScreamRx();
+
+  struct pollfd fds[1];
+  fds[0].fd = socket.fd_num();
+
+  while (true) {
+    SystemCall("poll", poll(fds, 1, -1));
+
+    /* Incoming RTP packet */
+    if (fds[0].revents | POLLIN) {
+      recvRtp(screamRx, socket);
+    }
+  }
 
   delete screamRx;
 
