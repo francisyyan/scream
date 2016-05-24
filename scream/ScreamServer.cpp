70042ae62c090937e86fed14dab63ad684b8082d
Francis <yuyan0@stanford.edu>
2016-05-23 17:43:59 -0700

    ScreamServer.cpp: use sendto() to send to client

--- scream/ScreamServer.cpp
+++ scream/ScreamServer.cpp
@@ -14,6 +14,8 @@
 using namespace std;
 
 const uint64_t tmax_ms = 15000; // run 15 seconds
+Address client_address;
+bool client_address_initialized = false;
 
 /* Send RTCP feedback */
 void sendRtcp(ScreamRx *screamRx, UDPSocket &socket)
@@ -27,7 +29,10 @@ void sendRtcp(ScreamRx *screamRx, UDPSocket &socket)
   if (screamRx->getFeedback(timestamp_us(), ssrc, 
       recv_timestamp_ms, ack_seq_num, num_loss)) {
     RtcpPacket rtcpPacket(ssrc, ack_seq_num, (uint16_t) num_loss, recv_timestamp_ms);
-    socket.send(rtcpPacket.to_string());
+    socket.sendto(client_address, rtcpPacket.to_string());
+
+    cerr << "Sent a RTCP packet acking sequence number " << ack_seq_num
+         << " at time " << recv_timestamp_ms << endl;
   }
 }
 
@@ -39,8 +44,18 @@ void recvRtp(ScreamRx *screamRx, UDPSocket &socket, Timerfd &feedbackTimer)
   UDPSocket::received_datagram recd = socket.recv();
   uint64_t recv_timestamp_us = recd.timestamp * 1000;
 
+  if (!client_address_initialized) {
+    client_address_initialized = true;
+    client_address = recd.source_address;
+  }
+
   /* Assemble RTP packet */
   RtpPacket rtpPacket(recd.payload);
+
+  cerr << "Received a RTP packet of size " << rtpPacket.payload.size()
+       << " with sequence number " << rtpPacket.header.seq_num
+       << " at time " << recd.timestamp << endl;
+
   screamRx->receive(recv_timestamp_us, 0, rtpPacket.header.ssrc, 
                     (int) rtpPacket.payload.size(), rtpPacket.header.seq_num); 
 
